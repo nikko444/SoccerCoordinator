a78593137b9a489f9afe023879141402543524fc
Nick Fialkowski <nikko444@gmail.com>
2017-11-06 00:19:53 -0500

    Created the "players" dictionary

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -1,5 +1,38 @@
-//: Playground - noun: a place where people can play
+//Techdegree project #1
 
 import UIKit
 
-var str = "Hello, playground"
+let players: [[String: String]] = [
+    ["name": "Joe Smith", "height":"42", "experience":"YES", "guardianName":"Jim and Jan Smith"],
+    ["name": "Jill Tanner", "height":"36", "experience":"YES", "guardianName":"Clara Tanner"],
+    ["name": "Bill Bon", "height":"43", "experience":"YES", "guardianName":"Sara and Jenny Bon"],
+    ["name": "Eva Gordon", "height":"45", "experience":"NO", "guardianName":"Wendy and Mike Gordon"],
+    ["name": "Matt Gill", "height":"40", "experience":"NO", "guardianName":"Charles and Sylvia Gill"],
+    ["name": "Kimmy Stein", "height":"41", "experience":"NO", "guardianName":"Bill and Hillary Stein"],
+    ["name": "Sammy Adams", "height":"45", "experience":"NO", "guardianName":"Jeff Adams"],
+    ["name": "Karl Saygan", "height":"42", "experience":"YES", "guardianName":"Heather Bledsoe"],
+    ["name": "Suzane Greenberg", "height":"44", "experience":"YES", "guardianName":"Henrietta Dumas"],
+    ["name": "Sal Dali", "height":"41", "experience":"NO", "guardianName":"Gala Dali"],
+    ["name": "Joe Kavalier", "height":"39", "experience":"NO", "guardianName":"Sam and Elaine Kavalier"],
+    ["name": "Ben Finkelstein", "height":"44", "experience":"NO", "guardianName":"Aaron and Jill Finkelstein"],
+    ["name": "Diego Soto", "height":"41", "experience":"YES", "guardianName":"Robin and Sarika Soto"],
+    ["name": "Chloe Alaska", "height":"47", "experience":"NO", "guardianName":"David and Jamie Alaska"],
+    ["name": "Arnold Willis", "height":"43", "experience":"NO", "guardianName":"Claire Willis"],
+    ["name": "Phillip Helm", "height":"44", "experience":"YES", "guardianName":"Thomas Helm and Eva Jones"],
+    ["name": "Les Clay", "height":"42", "experience":"YES", "guardianName":"Wynonna Brown"],
+    ["name": "Herschel Krustofski", "height":"45", "experience":"YES", "guardianName":"Hyman and Rachel Krustofski"]
+                                    ]
+for player in players {
+    
+}
+
+
+func isExperienced (_ player: [String: String]) -> Bool{
+    return true
+}
+
+func numberOfExperienced(_ players: [[String: String]]) -> (Is: Int){
+    for player in players {
+        if 
+    }
+}
